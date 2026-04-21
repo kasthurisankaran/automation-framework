078443b4a9705b90305ce60c8c4ae96d013214b7
KasthuriSankaran <kasthurisankaran2003@gmail.com>
2026-04-21 16:20:21 +0530

    Implemented drag-and-drop using JavaScriptExecutor, fixed element text validation, improved test stability

--- Pages/DragDropPage.cs
+++ Pages/DragDropPage.cs
@@ -1,31 +1,80 @@
+﻿using OpenQA.Selenium;
 using AutomationFramework.Utilities;
-using OpenQA.Selenium;
-namespace AutomationFramework.Pages;
 
-public class DragDropPage
+namespace AutomationFramework.Pages
 {
-    IWebDriver driver;
-    WaitHelper waitHelper;
-    ActionsHelper actionsHelper;
+    public class DragDropPage
+    {
+        IWebDriver driver;
+        WaitHelper waitHelper;
 
-    public DragDropPage(IWebDriver driver)
-    {
-        this.driver = driver;
-        waitHelper = new WaitHelper(driver);
-        actionsHelper = new ActionsHelper(driver);
-    }
-    IWebElement source => waitHelper.WaitForElementVisible(By.Id("column-a"));
-    IWebElement target => waitHelper.WaitForElementVisible(By.Id("column-b"));
-    public void PerformDragAndDrop()
-    {
-        actionsHelper.DragAndDrop(source,target);
-    }
-    public string GetSourceText()
-    {
-        return source.Text;
-    }
-    public string GetTargetText()
-    {
-        return target.Text;
+        public DragDropPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            waitHelper = new WaitHelper(driver);
+        }
+
+        IWebElement source => waitHelper.WaitForElementVisible(By.Id("column-a"));
+        IWebElement target => waitHelper.WaitForElementVisible(By.Id("column-b"));
+        public void DragAndDrop()
+        {
+            DragAndDropUsingJS();
+        }
+        public void DragAndDropUsingJS()
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+
+            string script = @"
+                function createEvent(typeOfEvent) {
+                    var event = document.createEvent('CustomEvent');
+                    event.initCustomEvent(typeOfEvent, true, true, null);
+                    event.dataTransfer = {
+                        data: {},
+                        setData: function(key, value) {
+                            this.data[key] = value;
+                        },
+                        getData: function(key) {
+                            return this.data[key];
+                        }
+                    };
+                    return event;
+                }
+
+                function dispatchEvent(element, event, transferData) {
+                    if (transferData !== undefined) {
+                        event.dataTransfer = transferData;
+                    }
+                    if (element.dispatchEvent) {
+                        element.dispatchEvent(event);
+                    } else if (element.fireEvent) {
+                        element.fireEvent('on' + event.type, event);
+                    }
+                }
+
+                function simulateDragDrop(sourceNode, destinationNode) {
+                    var dragStartEvent = createEvent('dragstart');
+                    dispatchEvent(sourceNode, dragStartEvent);
+
+                    var dropEvent = createEvent('drop');
+                    dispatchEvent(destinationNode, dropEvent, dragStartEvent.dataTransfer);
+
+                    var dragEndEvent = createEvent('dragend');
+                    dispatchEvent(sourceNode, dragEndEvent, dropEvent.dataTransfer);
+                }
+
+                simulateDragDrop(arguments[0], arguments[1]);
+            ";
+
+            js.ExecuteScript(script, source, target);
+        }
+
+        public string GetSourceText()
+        {
+            return source.Text;
+        }
+        public string GetTargetText()
+        {
+            return target.Text;
+        }
     }
 }
--- Tests/DragDropTest.cs
+++ Tests/DragDropTest.cs
@@ -1,31 +1,37 @@
 using AutomationFramework.Pages;
 using AutomationFramework.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-namespace AutomationFramework;
 
-[TestClass]
-public class DragDropTest:BaseClass
+namespace AutomationFramework.Tests
 {
-    DragDropPage dragDropPage;
+    [TestClass]
+    public class DragDropTest : BaseClass
+    {
+        DragDropPage dragDropPage;
 
-    [TestInitialize]
-    public void Setup()
-    {
-        StartBrowser();
-        dragDropPage = new DragDropPage(driver);
-    }
-    [TestMethod]
-    public void VerifyDragDropFunctionality()
-    {
-        dragDropPage.PerformDragAndDrop();
-        string sourceText = dragDropPage.GetSourceText();
-        string targetText = dragDropPage.GetTargetText();
-        Assert.AreEqual("B", sourceText);
-        Assert.AreEqual("A", targetText);
-    }
-    [TestCleanup]
-    public void TearDown()
-    {
-        CloseBrowser(); 
+        [TestInitialize]
+        public void Setup()
+        {
+            StartBrowser();
+            dragDropPage = new DragDropPage(driver);
+        }
+
+        [TestMethod]
+        public void VerifyDragAndDrop()
+        {
+            dragDropPage.DragAndDrop();
+
+            string sourceText = dragDropPage.GetSourceText();
+            string targetText = dragDropPage.GetTargetText();
+
+            Assert.AreEqual("B", sourceText);
+            Assert.AreEqual("A", targetText);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            CloseBrowser();
+        }
     }
 }
